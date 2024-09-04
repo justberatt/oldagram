065c3c8a7232f2ba63d93caf7db3db9d3659c897
justberatt <berat.sakipi@gmail.com>
2024-09-05 01:41:24 +0200

    Optimized the CSS for the DRY principle. Also changed the app so that is renders the data dynamically based on an array of objects.

--- index.js
+++ index.js
@@ -28,3 +28,38 @@ const posts = [
     }
 ]
 
+const ulEl = document.querySelector('#ul-el');
+
+const render = () => {
+    let listItems = ""; // we populate then render so that we do not render for every 'for' cycle
+    for (let i=0; i<posts.length; i++) {
+        listItems += `
+            <li>
+                <div class="user-info">
+                    <img src="${posts[i].avatar}" class="user-avatar" alt="portrait of ${posts[i].name}">
+                    <div class="user-info__paragraphs">
+                        <p class="name">${posts[i].name}</p>
+                        <p class="location">${posts[i].location}</p>
+                    </div>
+                </div>
+                 <div class="post">
+                            <img src="${posts[i].post}" class="post-image" alt="painting of ${posts[i].name}">
+                            <div class="post-icons">
+                                <img src="images/icon-heart.png" class="heart-icon" alt="heart icon">
+                                <img src="images/icon-comment.png" class="comment-icon" alt="comment icon">
+                                <img src="images/icon-dm.png" class="share-icon" alt="share icon">
+                            </div>
+                            <p class="likes-counter">${posts[i].likes}</p>
+                            <div class="user-caption">
+                                <p class="username">${posts[i].username}</p>
+                                <p class="user-caption__text">${posts[i].comment}</p>
+                            </div>
+                        </div>
+            </li>
+        `
+    }
+
+    ulEl.innerHTML = listItems;
+}
+
+render();
