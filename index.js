f715625326bd907549d8e89c709883309d2df740
justberatt <berat.sakipi@gmail.com>
2024-09-05 04:04:17 +0200

    implemented the 'like' button or whenever you click twice on a picture, the like number increases by one

--- index.js
+++ index.js
@@ -43,18 +43,18 @@ const render = () => {
                     </div>
                 </div>
                  <div class="post">
-                            <img src="${posts[i].post}" class="post-image" alt="painting of ${posts[i].name}">
-                            <div class="post-icons">
-                                <img src="images/icon-heart.png" class="heart-icon" alt="heart icon">
-                                <img src="images/icon-comment.png" class="comment-icon" alt="comment icon">
-                                <img src="images/icon-dm.png" class="share-icon" alt="share icon">
-                            </div>
-                            <p class="likes-counter">${posts[i].likes}</p>
-                            <div class="user-caption">
-                                <p class="username">${posts[i].username}</p>
-                                <p class="user-caption__text">${posts[i].comment}</p>
-                            </div>
-                        </div>
+                    <img src="${posts[i].post}" class="post-image" alt="painting of ${posts[i].name}">
+                    <div class="post-icons">
+                        <img src="images/icon-heart.png" class="btn heart-icon" alt="heart icon">
+                        <img src="images/icon-comment.png" class="btn comment-icon" alt="comment icon">
+                        <img src="images/icon-dm.png" class="btn share-icon" alt="share icon">
+                    </div>
+                    <p class="likes-counter">${posts[i].likes}</p>
+                    <div class="user-caption">
+                        <p class="username">${posts[i].username}</p>
+                        <p class="user-caption__text">${posts[i].comment}</p>
+                    </div>
+                </div>
             </li>
         `
     }
@@ -63,3 +63,31 @@ const render = () => {
 }
 
 render();
+
+const likes = document.querySelector('.likes-counter');
+
+ulEl.addEventListener('click', (e) => {
+    const target = e.target;
+    
+    // Check if the target is a heart icon
+    if (target.classList.contains('heart-icon')) {
+        const post = target.closest('.post'); // Get the closest post element
+        const likesCounter = post.querySelector('.likes-counter'); // Get the likes counter element
+        let likes = parseInt(likesCounter.textContent); // Get the current number of likes
+        likes++; // Increment the likes
+        likesCounter.textContent = likes; // Update the likes counter in the DOM
+    }
+})
+
+ulEl.addEventListener('dblclick', (e) => {
+    const target = e.target;
+    
+    // Check if the target is a heart icon
+    if (target.classList.contains('post-image')) {
+        const post = target.closest('.post'); // Get the closest post element
+        const likesCounter = post.querySelector('.likes-counter'); // Get the likes counter element
+        let likes = parseInt(likesCounter.textContent); // Get the current number of likes
+        likes++; // Increment the likes
+        likesCounter.textContent = likes; // Update the likes counter in the DOM
+    }
+})
